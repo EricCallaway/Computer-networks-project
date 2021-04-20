711d70c5baa601c4e6ede36f9a2164a4f9c682d6
Eric Callaway <ericcallaway5@gmail.com>
2021-04-20 13:56:36 -0500

    Fifth commit

--- main.py
+++ main.py
@@ -8,17 +8,14 @@ from tkinter import *
 from PIL import ImageTk,Image
 from mpl_finance import candlestick_ohlc
 
+# Main function
 def main():
     tickers = get_data()
-    selection(tickers)
-    candlestick()
-
-
-
-
+    user_selection = selection(tickers)
+    candlestick(user_selection)
+    
 # Select a company
 def selection(tickers):
-
     root = Tk()
     root.title("Eric's stock picking program!")
 
@@ -27,71 +24,67 @@ def selection(tickers):
 
     company = StringVar()
     company.set(tickers[0])
-
     drop = OptionMenu(root, company, *tickers)
     drop.pack()
 
-    myButton = Button(root, text="Show Selection", command=show).pack()
 
+    #Wanted to include the functionality to choose the stock you want from the drop down menu, but I could not figure out how to. 
+    #myButton = Button(root, text="Show Selection", command=show).pack()
+    #submit_button = Button(root, text="Submit", command=submit(e.get())).pack()
+    #e = Entry(root, borderwidth=5, width=50)
+    #e.pack()
+    #e.insert(0, 'Enter the ticker symbol of the company you want to analyze. ')
+    user_selection = input('Enter the ticker symbol of the stock you want to analyze: ')
     root.mainloop()
-
-
+    return user_selection
+    
 # Get data from wikipedia
 def get_data():
-
     html = requests.get('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-
     soup = bs.BeautifulSoup(html.text)
-
     tickers = []
-
     table = soup.find('table', {'class': 'wikitable sortable'})
     rows = table.findAll('tr')[1:]
-
     for row in rows:
         ticker = row.findAll('td')[0].text
         tickers.append(ticker[:-1])
-
-    
-    print(tickers)
+    #print(tickers)
     return tickers
 
 #Define time frame
-
-def candlestick():
+def candlestick(ticker):
     start = dt.datetime(2020,1,1)
     end = dt.datetime.now()
+    print('Company to be analyzed: ', ticker)
+    print('Analyzing...')
 
     #Load
-
     data = web.DataReader(ticker, 'yahoo', start, end)
 
-    #print(data.columns)
-
     # Restructure Data
-
     data = data[['Open', 'High', 'Low', 'Close']]
-
     data.reset_index(inplace=True)
     data['Date'] = data['Date'].map(mdates.date2num)
 
     # Visualization
-
     ax = plt.subplot()
     ax.grid(True)
     ax.set_axisbelow(True)
-    ax.set_title(ticker, ', Share price', color='white')
+    ax.set_title('{} Share price'.format(ticker), color='white')
     ax.set_facecolor('black')
     ax.figure.set_facecolor('#121212')
     ax.tick_params(axis='x', colors='white')
     ax.tick_params(axis='y', colors='white')
     ax.xaxis_date()
-
     candlestick_ohlc(ax, data.values, width=0.5, colorup='g')
     plt.show()
-
     print(data.head())
 
+# Submit button
+def submit(user_selection):
+    candlestick(user_selection)
+
+# Calling main function
 main()
 
 
